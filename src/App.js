ada7e308d2c87969e4f4e4cfc8c32d32e2306c1e
Tim Romanowski <timromanowski15@gmail.com>
2022-03-03 14:19:04 +0100

    Update and delete starting files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+
+import List from './List';
+
+const shoppingList = [
+  {
+    id: 0,
+    name: 'Eggs',
+    number: 12,
+    inCart: false,
+  },
+  {
+    id: 1,
+    name: 'Milk',
+    number: 8,
+    inCart: false,
+  },
+  {
+    id: 2,
+    name: 'Flour',
+    number: 1,
+    inCart: false,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Shopping List</h1>
+    </>
   );
 }
 
